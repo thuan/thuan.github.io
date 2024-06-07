f195f5134bda3f001ee244c0fc78cb417dfc734b
thuansaraiva <thuansaraiva@gmail.com>
2024-06-07 04:51:26 +0000

    astro-blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thuan.github.io',
-  base: '/',
+  base: '/astro-blog',
   integrations: [mdx(), sitemap(), tailwind()]
 });
